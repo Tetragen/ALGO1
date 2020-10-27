be768c829048e077df7c386a24c71ba6f6394636
nico <nicolaslehir@gmail.com>
2020-10-27 19:57:49 +0100

    edit factorial

--- code/recursion/factorial_rec.py
+++ code/recursion/factorial_rec.py
@@ -1,6 +1,8 @@
 """
 Compute the factorial with a recursion
 """
+import math
+from termcolor import colored
 
 
 def factorial_rec(n):
@@ -10,4 +12,24 @@ def factorial_rec(n):
         return factorial_rec(n - 1) * n
 
 
-print(factorial_rec(10))
+def test_function(to_test):
+    """Function to test recursive method
+    by comparing with the factorial function
+    from the math module
+
+    :to_test: int
+
+    """
+    recursive = factorial_rec(to_test)
+    math_function = math.factorial(to_test)
+    if recursive == math_function:
+        print(colored(f"n={to_test} result ok: {math_function}", "blue", attrs=["bold"]))
+    else:
+        print(colored(f"n={to_test} wrong result: {math_function} vs "+
+                      f"{recursive} (recursive)",
+                      "yellow",
+                      attrs=["bold"]))
+
+
+test_function(10)
+test_function(5)
