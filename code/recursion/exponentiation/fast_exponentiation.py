84035c150bbe1916168e77953ba1da1d16d18db9
nico <nicolaslehir@gmail.com>
2020-10-27 20:04:52 +0100

    edit fast exp

--- code/recursion/exponentiation/fast_exponentiation.py
+++ code/recursion/exponentiation/fast_exponentiation.py
@@ -1,6 +1,7 @@
 """
     Fast exponentiation algorithm
 """
+from termcolor import colored
 
 
 def fast_exponentiation(a, n):
@@ -12,4 +13,25 @@ def fast_exponentiation(a, n):
         return a * fast_exponentiation(a, n // 2)**2
 
 
-print(fast_exponentiation(5, 300000))
+def test_function(a, n):
+    """Function to test recursive method
+    by comparing with the factorial function
+    from the math module
+
+    :to_test: int
+
+    """
+    recursive = fast_exponentiation(a, n)
+    math_function = pow(a, n)
+    if recursive == math_function:
+        print(
+            colored(f"a={a}, n={n} result ok: {math_function}", "blue", attrs=["bold"]))
+    else:
+        print(colored(f"a={a}, n={n} wrong result: {math_function} vs " +
+                      f"{recursive} (recursive)",
+                      "yellow",
+                      attrs=["bold"]))
+
+
+test_function(13, 7)
+test_function(5, 20)
