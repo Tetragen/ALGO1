20961dae3fd3db4416b8576cef432a14914f1158
nico <nicolaslehir@gmail.com>
2020-10-27 19:57:10 +0100

    edit factorial

--- make_exercises.sh
+++ make_exercises.sh
@@ -1,2 +1,3 @@
 git diff exercise..correction -- code/crypto_intro/* > ../exercices/crypto_intro.txt
 git diff exercise..correction -- code/rsa/* > ../exercices/rsa.txt
+git diff exercise..correction -- code/recursion/* > ../exercices/recursion.txt
